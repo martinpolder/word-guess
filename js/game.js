4210f50ced74b24677dac7ee1cb16193b82a77a9
Martin Polder <martinpolder@gmail.com>
2019-06-28 13:07:53 -0400

    function to select the word from array and create it with blanks completed

--- js/game.js
+++ js/game.js
@@ -1,73 +1,43 @@
 
 // Array for all possible correct words
-var wordBank = ["piss"];
+var wordBank = ["piss", "flowers"];
 
 
 // variables
 var randomWord = "";
 var wordLetters = [];
 var blanks = 0;
-var correctLetters = [];
+var lettersAndBlanks = [];
 var wrongLetters = [];
 var guessesRemaining = 9;
 
 
+window.onclick = function game() {
 // CPU randomly chooses word from the bank for the user to solve 
-var wordSolve = wordBank[Math.floor(Math.random() * wordBank.length)];
+var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+// this splits the randomly chosen word into seperate arrays by letter and put into a new array
+wordLetters = randomWord.split("");
+
+// blanks is set to the length of the word 
+blanks = wordLetters.length;
+
+// loop that makes " _ " for each letter in blanks and pushes it/adds it to the lettersandblanks array
+for (var i = 0; i < blanks; i++) {
+    lettersAndBlanks.push("_");
+    
+}
+
+// testing
+console.log(randomWord);
+console.log(wordLetters);
+console.log(blanks);
+console.log(lettersAndBlanks);
+// it works!
+
+}
 
 
 
 
 
-
-// This function is run whenever the user presses a key.
-document.onkeyup = function (event) {
-
-    // Determines which key was pressed.
-    var userGuess = event.key;
-
-
-
-
-
-
-    // This loops the function so the user can play again with another randomly chosen word
-    var answerArray = [];
-    for (var i = 0; i < wordSolve.length; i++) {
-        answerArray[i] = "_"
-    }
-
-
-
-    while (remainingLetters > 0) {
-        // Show the player their progress alert(answerArray.join(" "));
-
-        console.log(answerArray.join(" "));
-
-        // Get a guess from the player
-
-
-        if (userGuess) {
-
-     else if (userGuess.length !== 1) {
-                alert("Please enter a single letter.");
-            } else {
-                // Update the game state with the guess
-
-
-
-                // The end of the game loop
-            }
-        };
-
-
-
-    };
-
-
-
-
-
-
-    //  Console display 
-    console.log(answerArray.join(" "))
