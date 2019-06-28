823d1402e5d522ab1bd196dd4ef4e715e6b9336d
Martin Polder <martinpolder@gmail.com>
2019-06-28 12:49:58 -0400

    variables created

--- js/game.js
+++ js/game.js
@@ -2,18 +2,20 @@
 // Array for all possible correct words
 var wordBank = ["piss"];
 
+
+// variables
+var randomWord = "";
+var wordLetters = [];
+var blanks = 0;
+var correctLetters = [];
+var wrongLetters = [];
+var guessesRemaining = 9;
+
+
 // CPU randomly chooses word from the bank for the user to solve 
 var wordSolve = wordBank[Math.floor(Math.random() * wordBank.length)];
 
 
-// variable for wrong guesses, starts at 0
-var wrongLetter = 9;
-
-
-
-// variables that reference html
-
-// var randomWord document. not finished
 
 
 
@@ -29,38 +31,38 @@ document.onkeyup = function (event) {
 
 
 
-// This loops the function so the user can play again with another randomly chosen word
-var answerArray = [];
-for (var i = 0; i < wordSolve.length; i++) {
-    answerArray[i] = "_"
-}
+    // This loops the function so the user can play again with another randomly chosen word
+    var answerArray = [];
+    for (var i = 0; i < wordSolve.length; i++) {
+        answerArray[i] = "_"
+    }
 
 
 
-while (remainingLetters > 0) {
-    // Show the player their progress alert(answerArray.join(" "));
+    while (remainingLetters > 0) {
+        // Show the player their progress alert(answerArray.join(" "));
 
-    console.log(answerArray.join(" "));
+        console.log(answerArray.join(" "));
 
-    // Get a guess from the player
+        // Get a guess from the player
 
 
-    if (userGuess ) {
+        if (userGuess) {
 
      else if (userGuess.length !== 1) {
-        alert("Please enter a single letter.");
-    } else {
-        // Update the game state with the guess
+                alert("Please enter a single letter.");
+            } else {
+                // Update the game state with the guess
 
 
 
-        // The end of the game loop
-    }
+                // The end of the game loop
+            }
+        };
+
+
+
     };
-
-
-
-};
 
 
 
